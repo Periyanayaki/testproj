912f5a833edd9d36cdac2cd7af4c4e77ef656b36
Periyanayaki <pv0076416@techmahindra.com>
2014-03-05 20:27:25 +0530

    Creating a repository and pushing my project to remote

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testproj::Application.config.secret_token = 'df8bf3a0968ad6936e1ccca4c48f2bdfc883f9c341c04cfa0ab13af4d848b5bda07d802136c95291d4a46e6eb42cc86b0fea04cdd60ac35f72329e3139012f8d'
